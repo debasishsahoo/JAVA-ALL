de6254a00aacbf40a414ff5237cdc1ceddfa710b
Debasish Sahoo <debasish.sahoo96@gmail.com>
2022-12-16 00:58:22 +0530

    new update 16/12/22

---
+++ java/2.Datatype/BooleanDataType.java
@@ -0,0 +1,14 @@
+public class BooleanDataType {
+    public static void main(String []args){
+     boolean a=false;
+     boolean b=true;
+
+     if(b==true){
+        System.out.println("Value of b:"+b);
+     }
+     if(a==false){
+        System.out.println("Value of a:"+a);
+
+     }
+    }
+}
---
+++ java/2.Datatype/ByteDataType.java
@@ -0,0 +1,16 @@
+public class ByteDataType {
+    public static void main(String [] args){
+        byte a=127;
+        System.out.println(a);
+
+        // byte b=128;
+        // System.out.println(b);
+
+        // byte c=-129;
+        // System.out.println(c);
+
+        byte d=-128;
+        System.out.println(d);
+    }
+    
+}
