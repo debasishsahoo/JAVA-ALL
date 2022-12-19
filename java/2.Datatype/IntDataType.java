6123ce57367471310c9eae047a0c1a1bcf2e81dd
Debasish Sahoo <debasish.sahoo96@gmail.com>
2022-12-20 00:15:04 +0530

    New update 20/12/22

--- java/2.Datatype/IntDataType.java
+++ java/2.Datatype/IntDataType.java
@@ -1,3 +1,10 @@
 public class IntDataType {
+    public static void main(String []args){
+        int a = 2147483648;
+        System.out.println("Value of a:"+a);
+        int b = -2147483649;
+        System.out.println("Value of b:"+b);
+
+    }
     
 }
